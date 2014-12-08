ddc19570b7f6458532853c1de4ac9f8d9056398d
Amy <Dohyun@Amyui-MacBook-Air.local>
2014-12-08 20:40:25 +0900

    울지말고 천천히 얘기해봐

---
+++ Sunsal/AlbumManager.swift
@@ -0,0 +1,56 @@
+//
+//  AlbumManager.swift
+//  Sunsal
+//
+//  Created by Amy on 2014. 11. 10..
+//  Copyright (c) 2014년 Baekjoon Choi. All rights reserved.
+//
+
+import UIKit
+
+class AlbumManager : NSObject, MWPhotoBrowserDelegate{
+    
+    var imageFiles : [MWPhoto!] = []
+    var thumbnailFiles : [MWPhoto!] = []
+
+    
+    class var sharedManager : AlbumManager {
+        struct Static {
+            static let instance : AlbumManager = AlbumManager();
+        }
+        return Static.instance;
+    }
+    
+    override init() {
+        super.init()
+        loadFile();
+    }
+    func loadFile() {
+        var fileName = NSBundle.mainBundle().pathForResource("codershigh", ofType: "json")!;
+   //     NSLog("%@", filename);
+        
+        var fileData = NSData(contentsOfFile: fileName)!;
+        if let a = NSJSONSerialization.JSONObjectWithData(fileData, options: NSJSONReadingOptions.allZeros, error: nil) as? [String] {
+           // NSLog("%@",a);
+        //imageFileNames = a;
+            
+            for f in a {
+                var url  = "https://www.acmicpc.net" + f;
+                var thumbnailUrl = url.stringByReplacingOccurrencesOfString(".jpg", withString: "_thumb.jpg", options: NSStringCompareOptions.allZeros , range: nil)
+                var photo = MWPhoto(URL: NSURL(string: url)!)
+                var thumbnail = MWPhoto(URL: NSURL(string: thumbnailUrl)!)
+                imageFiles += [photo];
+                thumbnailFiles += [thumbnail];
+            }
+        }
+    }
+    func numberOfPhotosInPhotoBrowser(photoBrowser: MWPhotoBrowser!) -> UInt {
+        return UInt(imageFiles.count);
+    }
+    func photoBrowser(photoBrowser: MWPhotoBrowser!, photoAtIndex index: UInt) -> MWPhotoProtocol! {
+        return imageFiles[Int(index)]
+    }
+    func photoBrowser(photoBrowser: MWPhotoBrowser!, thumbPhotoAtIndex index: UInt) -> MWPhotoProtocol! {
+        return thumbnailFiles[Int(index)]
+    }
+}
---
+++ Sunsal/MainViewController.swift
@@ -0,0 +1,82 @@
+//
+//  MainViewController.swift
+//  Sunsal
+//
+//  Created by Baekjoon Choi on 11/6/14.
+//  Copyright (c) 2014 Baekjoon Choi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    @IBOutlet weak var hudCell: UITableViewCell!
+    @IBOutlet weak var alertCell: UITableViewCell!
+    @IBOutlet weak var actionSheetCell: UITableViewCell!
+    @IBOutlet weak var photoViewCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AlbumManager.sharedManager;
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            if cell == hudCell {
+                showHUD();
+            } else if cell == alertCell {
+                showAlert(UIAlertControllerStyle.Alert)
+            } else if cell == actionSheetCell {
+                showAlert(UIAlertControllerStyle.ActionSheet)
+            } else if cell == photoViewCell {
+                photoView()
+            }
+        }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true);
+    }
+    func photoView() {
+        var browser = MWPhotoBrowser(delegate: AlbumManager.sharedManager);
+        
+        browser.displayActionButton = true;
+        browser.displayNavArrows = false;
+        browser.displaySelectionButtons = false;
+        browser.zoomPhotosToFill = true;
+        browser.alwaysShowControls = false;
+        browser.enableGrid = true;
+        browser.startOnGrid = true;
+        browser.enableSwipeToDismiss = true;
+    
+        self.navigationController!.pushViewController(browser, animated: true)
+    }
+    
+    func showHUD() {
+        MBProgressHUD.showHUDAddedTo(navigationController?.view, animated: true);
+        NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: "hideHUD", userInfo: nil, repeats: false);
+    }
+    
+    func hideHUD() {
+        MBProgressHUD.hideHUDForView(navigationController?.view, animated: true);
+    }
+    
+    func showAlert(alertType: UIAlertControllerStyle) {
+        var alertController = UIAlertController(title: "안녕하세요", message: "나는 마스터입니다", preferredStyle: alertType);
+        
+        alertController.addAction(UIAlertAction(title: "꺼주세요", style: .Cancel, handler: { (alertAction) -> Void in
+            
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "끄지마요", style: .Default, handler: { (alertAction) -> Void in
+            self.presentViewController(alertController, animated: true, completion: nil);
+        }))
+        
+        presentViewController(alertController, animated: true, completion: nil);
+        
+    }
+    
+}
